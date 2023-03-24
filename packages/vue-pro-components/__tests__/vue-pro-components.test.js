c645cdcb1d500bbfe614f6907fa6fb4e1cc4fcb5
zhukaijie <zhukaijie@xun-ao.com>
2023-03-24 14:47:33 +0800

    chore: 先讲代码提交到远程，方便后续测试lerna version

---
+++ packages/vue-pro-components/__tests__/vue-pro-components.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const vueProComponents = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(vueProComponents(), 'Hello from vueProComponents');
+console.info('vueProComponents tests passed');
---
+++ packages/vue-pro-components/lib/vue-pro-components.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = vueProComponents;
+
+function vueProComponents() {
+  return 'Hello from vueProComponents';
+}
